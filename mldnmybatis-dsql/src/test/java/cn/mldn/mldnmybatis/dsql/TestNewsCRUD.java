c3172a1c7837128ebdd57830a2c6b5c62db15720
mldn <mldnqa@163.com>
2017-10-23 15:48:51 +0800

    实现choose、when、where、otherwise定义与使用。

--- mldnmybatis-dsql/src/test/java/cn/mldn/mldnmybatis/dsql/TestNewsCRUD.java
+++ mldnmybatis-dsql/src/test/java/cn/mldn/mldnmybatis/dsql/TestNewsCRUD.java
@@ -17,11 +17,27 @@ public class TestNewsCRUD {
 	static {
 		rand = random.nextInt(Integer.MAX_VALUE);
 	}
+	
+	@Test
+	public void testNewsSplit() throws Exception {
+		Map<String,Object> map = new HashMap<String,Object>() ;
+		map.put("column","title") ;
+		map.put("keyWord", "%新闻%") ;
+		List<News> newsList = MyBatisSessionFactory.getSession().selectList("cn.mldn.mapping.NewsNS.findSplit",
+				map);
+		Iterator<News> iter = newsList.iterator();
+		while (iter.hasNext()) {
+			News vo = iter.next();
+			System.out.println(vo);
+		}
+		MyBatisSessionFactory.close();
+	}
  
 	@Test
 	public void testNewsList() throws Exception {
 		Map<String,Object> map = new HashMap<String,Object>() ;
 		map.put("title","约翰的一生都在约汉");
+		map.put("nid", 5) ;
 		List<News> newsList = MyBatisSessionFactory.getSession().selectList("cn.mldn.mapping.NewsNS.findAll",
 				map);
 		Iterator<News> iter = newsList.iterator();
