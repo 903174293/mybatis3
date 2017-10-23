b957901a455ef9df7e4c8d8aff18d108d41ecc09
mldn <mldnqa@163.com>
2017-10-23 11:52:03 +0800

    实现数据的更新处理操作，分析了insert()、delete()、update()三个方法的使用。

--- mldnmybatis-crud/src/test/java/cn/mldn/mldnmybatis/crud/TestNewsCRUD.java
+++ mldnmybatis-crud/src/test/java/cn/mldn/mldnmybatis/crud/TestNewsCRUD.java
@@ -17,12 +17,33 @@ public class TestNewsCRUD {
 	}
 
 	@Test
+	public void testNewsRemove() throws Exception {
+		long nid = 1 ;
+		int len = MyBatisSessionFactory.getSession().delete("cn.mldn.mapping.NewsNS.doRemove", nid);
+		MyBatisSessionFactory.getSession().commit();
+		MyBatisSessionFactory.close();
+		TestCase.assertEquals(len, 1); // 如果更新行数为1表示成功
+	}
+
+	@Test
+	public void testNewsEdit() throws Exception {
+		News vo = new News();
+		vo.setNid(3L);
+		vo.setTitle("约翰的一生都在约汉");
+		vo.setNote("感染了许多的疾病，人生很困苦，人生一定要保守。");
+		int len = MyBatisSessionFactory.getSession().update("cn.mldn.mapping.NewsNS.doEdit", vo);
+		MyBatisSessionFactory.getSession().commit();
+		MyBatisSessionFactory.close();
+		TestCase.assertEquals(len, 1); // 如果更新行数为1表示成功
+	}
+
+	@Test
 	public void testNewsAdd() throws Exception {
 		News vo = new News();
 		vo.setTitle("新闻标题 - " + rand);
 		vo.setPubdate(new Date());
 		vo.setNote("新闻内容 - " + rand);
-		int len = MyBatisSessionFactory.getSession().insert("cn.mldn.mapping.NewsNS.doCreate", vo);
+		int len = MyBatisSessionFactory.getSession().update("cn.mldn.mapping.NewsNS.doCreate", vo);
 		MyBatisSessionFactory.getSession().commit(); // 提交更新事务
 		TestCase.assertEquals(len, 1); // 如果更新行数为1表示成功
 		System.out.println(vo);
